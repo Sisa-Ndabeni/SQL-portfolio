2dec193f6e6a1938de4ae49e84c914059bea9041
Sisisa Ndabeni <125610154+Sisa-Ndabeni@users.noreply.github.com>
2025-05-22 20:34:56 +0200

    Update and rename queries.sql to customer-support-ticketing-system/queries.sql

--- customer-support-ticketing-system/queries.sql
+++ customer-support-ticketing-system/queries.sql
@@ -1,21 +1,21 @@
--- Total tickets handled by each agent
+--Total tickets handled by each agent
 SELECT agent_id, COUNT(*) AS total_tickets
 FROM Tickets
 GROUP BY agent_id;
 
--- Average resolution time per agent (in days)
+--Average resolution time per agent (in days)
 SELECT agent_id,
-       AVG(julianday(closed_at) - julianday(opened_at)) AS avg_resolution_days
+       AVG(julianday(closed_at) - jualianday(opened_at)) AS avg_resolution_days
 FROM Tickets
 WHERE status = 'Closed'
 GROUP BY agent_id;
 
--- Open vs closed tickets
-SELECT status, COUNT(*) AS count
+--Open vs Closed tickets
+SELECT status, COUNT(*) AS COUNT
 FROM Tickets
 GROUP BY status;
 
--- Customers with most tickets
+--Customers with most tickets
 SELECT customer_id, COUNT(*) AS ticket_count
 FROM Tickets
 GROUP BY customer_id
