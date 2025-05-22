bd02dfb3d4d3249ab6eb8fcea8dc30c5c297d0e5
Sisisa Ndabeni <125610154+Sisa-Ndabeni@users.noreply.github.com>
2025-05-22 20:26:36 +0200

    Update and rename insert_sample_data.sql to customer-support-ticketing-system/insert_sample_data.sql

---
+++ customer-support-ticketing-system/insert_sample_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO Agents VALUES
+  (1, 'Bree Vanderkemp', 'bree@support.com'),
+  (2, 'Gabriella Solis', 'gabby@support.com');
+
+INSERT INTO Customers VALUES
+  (1, 'Linda Sibiya', 'linda@example.com'),
+  (2, 'KB Williams', 'kay@example.com');
+
+INSERT INTO Tickets VALUES
+  (1, 1, 1, 'Login issue', 'Closed', '2025-01-05', '2025-01-06'),
+  (2, 2, 2, 'Payment failed', 'Open', '2024-11-10', NULL),
+  (3, 1, 1, 'App crashing', 'Closed', '2025-01-08', '2025-01-09');
